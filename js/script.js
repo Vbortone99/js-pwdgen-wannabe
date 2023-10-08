fc7eb9d634bb91e2136b9c5a53ec3dbe70af6d53
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-08 19:05:48 +0200

    first structure

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
-const fixed_number = 21;
+const fixed_number = ('21');
+console.log('fixed_number')
 
 let nome = prompt('nome');
 let cognome = prompt('cognome');
 let colore_preferito = prompt('colore')
 
+let message = nome + cognome + colore_preferito;
+console.log('message');
+alert(let=message + fixed_number)
 
