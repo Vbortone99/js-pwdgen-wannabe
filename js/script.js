78d69b407ff183c38d1ead475a40256007819a12
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-08 19:32:59 +0200

    final-complete

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const fixed_number = ('21');
 console.log('fixed_number')
 
-let nome = prompt('nome');
-let cognome = prompt('cognome');
-let colore_preferito = prompt('colore')
+let nome = prompt('Il tuo nome');
+let cognome = prompt('Il tuo cognome');
+let colore_preferito = prompt('Il tuo colore preferito')
 
-let message = nome + cognome + colore_preferito;
+let message =nome+ ' ' + cognome+ ' ' +colore_preferito;
 console.log('message');
-alert(let=message + fixed_number)
+// alert(let=message + fixed_number)
 
+document.getElementById('square').innerHTML = message + ' ' +fixed_number
